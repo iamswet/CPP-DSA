638fe5eb100d62de7976a9883275cb5765aac17c
Swet Singh <111109005+iamswet@users.noreply.github.com>
2023-11-16 02:11:02 +0530

    Update power.cpp

--- Recursion/power.cpp
+++ Recursion/power.cpp
@@ -9,6 +9,19 @@ int powernum(int n,int iter){
     return n*powernum(n,iter-1);
 }
 
+int fastpower(int n,int iter){
+    if(iter==0){
+        return 1;
+    }
+    int smallerans=fastpower(n,iter/2);
+    smallerans *=smallerans;
+
+    if(iter & 1){
+        return smallerans * n;
+    }
+    return smallerans;
+}
+
 int main(){
 
     cout<<powernum(2,4);
